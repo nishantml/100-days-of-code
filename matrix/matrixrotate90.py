ffb085545799b1ffc86326aea68a46f9a84a9fe8
nishant <nishantbhushan10@gmail.com>
2021-03-11 13:18:53 +0530

    rotate1

--- matrix/matrixrotate90.py
+++ matrix/matrixrotate90.py
@@ -23,6 +23,22 @@ class Solution:
             print(row)
 
 
+    def rotate1(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        matrixLen = len(matrix)
+
+        for i in range(matrixLen):
+            for j in range(i, matrixLen):
+                print(i, j)
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for i in range(matrixLen):
+            for j in range(matrixLen // 2):
+                matrix[i][j], matrix[i][matrixLen - 1 - j] = matrix[i][matrixLen - 1 - j], matrix[i][j]
+
+
 sol = Solution()
 sol.rotate([[1,2,3],[4,5,6],[7,8,9]])
 sol.rotate([[1,2,3,10],[4,5,6,11],[7,8,9,12]])
